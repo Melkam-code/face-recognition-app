cfc3369f27c6b2595e37f37b00a75b83ec30b603
melkam <melkammekonnen899@gmail.com>
2020-12-30 21:04:06 +0300

    Creating a css file for logo component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import React, { Component } from 'react'; 
 import Navigation from './components/navigation/navigation';
+import Logo from './components/logo/logo';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
         <Navigation />
+        <Logo />
       </div>
     );
   }
