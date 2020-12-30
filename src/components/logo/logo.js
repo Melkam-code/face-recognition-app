6eedf84b6a00a553a866095f8965a67d1522d569
melkam <melkammekonnen899@gmail.com>
2020-12-30 21:48:40 +0300

    Styling my brain logo

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Tilt from 'react-tilt';
 import './logo.css';
+import brain from './brain.png';
 
 const Logo = () => {
     return(
         <div className='ma4 mt0'>
             <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
-            <div className="Tilt-inner"> 👽 </div>
+            <div className="Tilt-inner"><img alt='logo' src={brain}/></div>
             </Tilt>
         </div>
     )
