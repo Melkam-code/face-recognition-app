154c323abeefeeab122cf5a079cfafae14cb49a2
melkam <melkammekonnen899@gmail.com>
2020-12-30 22:22:51 +0300

    Installing react-particles-js inorder to have amazing background and adding it in app.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,25 @@ import Navigation from './components/navigation/navigation';
 import Logo from './components/logo/logo';
 import ImageLinkForm from './components/imagelinkform/imagelinkform';
 import Rank from './components/rank/rank';
+import Particles from 'react-particles-js';
+
+const particleOptions = {
+  particles: {
+    number: {
+      value:100,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+}
+}
 
 class App extends Component {
   render(){
     return (
       <div className="App">
+       <Particles className='particles' params={particleOptions} />
         <Navigation />
         <Logo />
         <Rank />
