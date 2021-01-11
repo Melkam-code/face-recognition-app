38e9c7163abfd6a38e24c8f907703113aec7f75c
melkam <melkammekonnen899@gmail.com>
2021-01-11 14:10:52 +0300

    Build the bounding box to show faces

--- src/App.js
+++ src/App.js
@@ -48,10 +48,11 @@ class App extends Component {
   }
 
   calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
     const height = Number(image.height);
+    /* there is an issue with the left and right columns, they are wide and I don't know how to debug this shit! */
     return{
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
